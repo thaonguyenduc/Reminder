25e4da4f5f598e9d5a8df85db31a1eefb4ff2e17
Nguyen Thao <n_ducthao@yahoo.com>
2016-03-04 16:00:18 +0700

    initial comment using new project wizard

--- app/src/main/java/structis/com/reminders/ReminderActivity.java
+++ app/src/main/java/structis/com/reminders/ReminderActivity.java
@@ -2,6 +2,8 @@ package structis.com.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,5 +19,21 @@ public class ReminderActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.reminders_rows, R.id.row_text,
                 new String[]{"first record", "second record", "third record"}        );
         mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_new:
+                // create new Reminder
+                Log.d(getLocalClassName(),"create new Remidner");
+                return true;
+            case R.id.action_exit:
+                //exit
+                finish();
+                return false;
+            default:
+                return false;
+        }
     }
 }
